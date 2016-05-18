39ee10f005247bb4f505edc9ae4b07ca05a9e900
fmielke <1fabian.mielke@gmail.com>
2016-05-18 22:29:58 +0200

    update gitignore and basic sql statements

---
+++ teamordner/sql/00_create_db_shop.sql
@@ -0,0 +1,13 @@
+-- Database: "SHOP"
+
+-- DROP DATABASE "SHOP";
+
+CREATE DATABASE "SHOP"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'de_DE.UTF-8'
+       LC_CTYPE = 'de_DE.UTF-8'
+       CONNECTION LIMIT = -1;
+
+
