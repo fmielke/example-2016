c3a92151c4f02d27e0d33b4cb2ae3d9c7888b667
fmielke <1fabian.mielke@gmail.com>
2016-05-23 23:09:36 +0200

    tidy up a little bit

--- src/main/java/de/htwberlin/mae/Application.java
+++ src/main/java/de/htwberlin/mae/Application.java
@@ -1,18 +1,11 @@
 package de.htwberlin.mae;
 
 
-import java.util.Properties;
-
+import de.htwberlin.mae.config.AppConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-import de.htwberlin.mae.config.AppConfiguration;
 
 @SpringBootApplication
-@ComponentScan({"de.htwberlin.mae.model",
-				"de.htwberlin.mae.repository",
-				"de.htwberlin.mae"})
 public class Application {
 
     public static void main(String[] args) {
