a362d77d9802538437aacdd3e8c070769a12ff53
fmielke <1fabian.mielke@gmail.com>
2016-05-27 22:59:10 +0200

    creating tomcat access log and log4j2 error log

--- src/main/java/de/htwberlin/mae/config/AppConfiguration.java
+++ src/main/java/de/htwberlin/mae/config/AppConfiguration.java
@@ -1,5 +1,8 @@
 package de.htwberlin.mae.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Properties;
 
 public class AppConfiguration {
@@ -12,10 +15,12 @@ public class AppConfiguration {
 	 * SYSTEM_ENV is "heroku" if application runs in heroku or "local" if application runs local
 	 */
 	public static void setEnvironmentProperties(){
+		Logger logger = LoggerFactory.getLogger(AppConfiguration.class);
 		if(System.getenv("ENV_SYSTEM") == null) {
     		Properties props = System.getProperties();
     		props.setProperty("ENV_SYSTEM", "local");
     		System.out.println("ENV_SYSTEM: " + System.getProperty("ENV_SYSTEM"));
+			logger.info("ENV_SYSTEM: {}", System.getProperty("ENV_SYSTEM"));
     	}
     	else {
     		System.out.println("ENV_SYSTEM: " + System.getenv("ENV_SYSTEM"));
