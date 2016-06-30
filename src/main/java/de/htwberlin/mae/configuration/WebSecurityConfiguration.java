b0207e2eb836ec8a47a74dd2eec310d2ae580c4e
fmielke <1fabian.mielke@gmail.com>
2016-06-30 23:18:36 +0200

    oauth2 impl (tschia)

---
+++ src/main/java/de/htwberlin/mae/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,60 @@
+package de.htwberlin.mae.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import static de.htwberlin.mae.configuration.OAuth2Configuration.PERMISSION_NUTZER_DELETE;
+import static de.htwberlin.mae.configuration.OAuth2Configuration.PERMISSION_NUTZER_READ;
+import static de.htwberlin.mae.configuration.OAuth2Configuration.PERMISSION_NUTZER_WRITE;
+
+/**
+ * Created by fmielke on 29.06.16.
+ */
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    Logger log = LoggerFactory.getLogger(WebSecurityConfiguration.class);
+
+    @Override
+    @Bean
+    public AuthenticationManager    authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint((request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("reader")
+                .password("reader")
+                .authorities(PERMISSION_NUTZER_READ)
+                .and()
+                .withUser("writer")
+                .password("writer")
+                .authorities(PERMISSION_NUTZER_WRITE)
+                .and()
+                .withUser("deleter")
+                .password("deleter")
+                .authorities(PERMISSION_NUTZER_DELETE);
+    }
+}
